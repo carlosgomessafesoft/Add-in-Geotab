701b15435701f3fba7f0e76ca727b26f3b393d4e
carlosgomessafesoft <carlos.gomes@safesoft.com.br>
2023-11-20 16:01:44 -0300

    teste java miScript

--- Sensor-temperatura/miScript.js
+++ Sensor-temperatura/miScript.js
@@ -1,5 +1,12 @@
 console.log("Olá, Mundo! Este é o meu script JavaScript!");
 
+const button = document.querySelector("input");
+const paragraph = document.querySelector("p");
+
+button.addEventListener("click", mostrarFecha);
+
+
 function mostrarFecha() {
+  console.log("Botao pressionado")
   document.getElementById('demo').innerHTML = Date();
 }

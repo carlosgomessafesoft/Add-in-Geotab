1d4e9da94678c6d8ea18f8ba40c723f75782110b
carlosgomessafesoft <carlos.gomes@safesoft.com.br>
2023-11-21 10:43:10 -0300

    teste

--- Sensor-temperatura/myScript.js
+++ Sensor-temperatura/myScript.js
@@ -1,26 +1,11 @@
+console.log("Carregou o script");
 
-    console.log("Carregou o script");
+const button = document.querySelector("input");
+const paragraph = document.querySelector("p");
 
-    const button = document.querySelector("input");
-    const paragraph = document.querySelector("p");
+button.addEventListener("click", mostrarFecha);
 
-    button.addEventListener("click", mostrarFecha);
-
-    function mostrarFecha() {
-        console.log("Botao pressionado")
-        document.getElementById('demo').innerHTML = Date();
-    }
-    return {
-        initialize(api, state, callback) {
-            callback();
-            console.log("initialize")
-        },
-        focus(api, state) {
-            // User interface is available
-            console.log("focus")
-        },
-        blur(api, state) {
-            // Save any Add-In state
-            console.log("blur")
-        }
-    };
+function mostrarFecha() {
+    console.log("Botao pressionado")
+    document.getElementById('demo').innerHTML = Date();
+}

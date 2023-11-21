ba51226f9fba5f33b6e40b1a5593b518b510b0e9
carlosgomessafesoft <carlos.gomes@safesoft.com.br>
2023-11-21 17:35:37 -0300

    teste

--- Sensor-temperatura/scripts/main.js
+++ Sensor-temperatura/scripts/main.js
@@ -1,21 +1,24 @@
 "use strict";
-
+geotab.addin.SensorTemperatura = () => {
     document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("submit").addEventListener("click", function (event) {
             event.preventDefault();
             console.log("botão pressionado")
-            // api.multiCall (
-            //     [
-            //         ["Get", {"typeName": "Group", "search":{"name":"SensorTemperatura"}}],
-            //         ["Get", {"typeName": "Group",  "resultsLimit": 10}]
-            //     ],
-            //     function (result) {
-            //         if (result) {
-            //             document.getElementById("vehicleCount").innerHTML = result[0];
-            //             document.getElementById("userCount").innerHTML = result[1];
-            //         }
-            //     }, function (error) {
-            //         alert(error);
-            // });
         });
     });
+    
+    return {
+        initialize(api, state, callback) {
+            callback();
+            console.log("Inicialização da pagina")
+        },
+        focus(api, state) {
+            // User interface is available
+            console.log("focus")
+        },
+        blur(api, state) {
+            // Save any Add-In state
+            console.log("blur")
+        }
+    };
+};
